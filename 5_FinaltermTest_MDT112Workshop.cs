e897b5670acfcb2ce84a493b0a7ed30fe32cd4d7
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-06-13 16:59:44 +0700

    Add files via upload

---
+++ 5_FinaltermTest_MDT112Workshop.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace _5_FinaltermTest_MDT112Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.Write("");
+            a = Console.ReadLine();
+
+            string b;
+            Console.Write("");
+            b = Console.ReadLine();
+
+            string c;
+            Console.Write("");
+            c = Console.ReadLine();
+
+            string d;
+            Console.Write("");
+            d = Console.ReadLine();
+
+            string e;
+            Console.Write("");
+            e = Console.ReadLine();
+
+            string f;
+            Console.Write("");
+            f = Console.ReadLine();
+
+            string h;
+            Console.Write("");
+            h = Console.ReadLine();
+
+            string i;
+            Console.Write("");
+            i = Console.ReadLine();
+
+            string j;
+            Console.Write("");
+            j = Console.ReadLine();
+
+            string k;
+            Console.Write("");
+            k = Console.ReadLine();
+
+            string l;
+            Console.Write("");
+            l = Console.ReadLine();
+
+            string m;
+            Console.Write("");
+            m = Console.ReadLine();
+
+            string n;
+            Console.Write("");
+            n = Console.ReadLine();
+
+            Console.WriteLine("{0}", b);
+            Console.WriteLine("{0}", c);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", d);
+            Console.WriteLine("{0}", e);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", f);
+            Console.WriteLine("{0}", h);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", i);
+            Console.WriteLine("{0}", j);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", f);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", b);
+            Console.WriteLine();
+
+            Console.WriteLine("{0}", d);
+            Console.WriteLine();
+
+            Console.WriteLine("End");
+        }
+    }
+}
